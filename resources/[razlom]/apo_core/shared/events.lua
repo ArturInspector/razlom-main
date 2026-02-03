bc7efe85e416f8ad0a5e80afb6e78a7d4041b7ad
ArturInspector <bikk_artur@mail.ru>
2026-02-04 01:18:46 +0600

    chore(core): add events contract

---
+++ resources/[razlom]/apo_core/shared/events.lua
@@ -0,0 +1,24 @@
+Events = {
+    -- Player lifecycle
+    PLAYER_LOADED = 'apo:player:loaded',
+    PLAYER_SAVED = 'apo:player:saved',
+
+    -- Inventory
+    ITEM_ADDED = 'apo:inventory:itemAdded',
+    ITEM_REMOVED = 'apo:inventory:itemRemoved',
+    ITEM_USED = 'apo:inventory:itemUsed',
+
+    -- Combat
+    MOB_SPAWNED = 'apo:mobs:spawned',
+    MOB_DIED = 'apo:mobs:died',
+    MOB_DAMAGED = 'apo:mobs:damaged',
+
+    -- Signal
+    NOISE_CREATED = 'apo:signal:noise',
+    HOTSPOT_TRIGGERED = 'apo:signal:hotspot',
+
+    -- Progression
+    XP_GAINED = 'apo:progression:xpGained',
+    RANK_UP = 'apo:progression:rankUp'
+}
+
